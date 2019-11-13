4f1572718d55c96ebe484d100f4f1053287de1ec
Anirup Patnaik <anirup049@gmail.com>
2019-11-14 01:26:29 +0530

    Initial Commit

---
+++ AdvancedSwiftProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  AdvancedSwiftProject
+//
+//  Created by Anirup Patnaik on 14/11/19.
+//  Copyright © 2019 Virtuelabs. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
